0555848a3c9fabacf2120026b399b69a75b08315
Stefan Eady <stefan@stefaneady.com>
2019-11-28 08:14:37 +1300

    final

--- WebApplication2/My Project/Contact.aspx.vb
+++ WebApplication2/My Project/Contact.aspx.vb
@@ -1,4 +1,4 @@
-﻿Public Class Contact
+﻿Public Class contact
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
@@ -6,10 +6,10 @@
     End Sub
 
     Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        If TextBox8.Text IsNot "" And TextBox7.Text IsNot "" Then
-            MsgBox("Thanks We Will Be In Touch")
-        Else
+        If TextBox8.Text Is "" Or TextBox7.Text Is "" Then
             MsgBox("You Must Fill All Feilds")
+        Else
+            MsgBox("Thanks We Will Be In Touch")
         End If
     End Sub
 
